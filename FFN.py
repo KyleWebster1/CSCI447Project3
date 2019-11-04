d060a41e2f53577942abe55ea5df40edeb580d72
m-wintersteen <muniwintersteen@gmail.com>
2019-11-03 18:59:14 -0700

    Class functions created

--- FFN.py
+++ FFN.py
@@ -6,3 +6,36 @@
 
 import KNN
 import FFN
+
+class ff_neural_net:
+    def __init__(self,training_set,test_set,outputs,num_hidden_layers,num_hidden_nodes):
+        self.training_set = training_set
+        self.test_set = test_set
+        self.outputs = outputs
+        self.num_hidden_layers = num_hidden_layers
+        self.num_hidden_nodes = num_hidden_nodes
+        #TODO
+        #create network
+
+        #initialize weights to zero
+        
+    def train(self,training_rate,momentum):
+        #TODO
+    
+    def test(self):
+        result = None
+        #TODO
+        return result
+
+    def feed_forward(self, j):
+        #TODO
+
+    def back_prop(self, j, delta, training_rate, momentum)
+        result = None
+        #TODO
+        return result
+
+    def activation(self, i, j):
+        result = None
+        #TODO
+        return result
--- RBF.py
+++ RBF.py
@@ -6,3 +6,27 @@
 
 import KNN
 import FFN
+
+class rb_neural_net:
+    def __init__(self,training_set,test_set,outputs,gaussians):
+        self.training_set = training_set
+        self.test_set = test_set
+        self.outputs = outputs
+        self.gaussians = gaussians
+
+        #TODO
+        
+    def train(self, k, momentum):
+        #TODO
+
+    def test(self, k):
+        result = None
+        #TODO
+        return result
+
+    def activation(self, i, j):
+        result = None
+        #TODO
+        return result
+        
+    
