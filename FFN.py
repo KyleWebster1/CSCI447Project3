c798fe8f1998d4653f0a98f70fcf89f101c4686f
Kyle Webster <kyle1234webster@gmail.com>
2019-11-04 13:59:37 -0700

    Added comments and dot function

--- FFN.py
+++ FFN.py
@@ -7,35 +7,91 @@
 import KNN
 import FFN
 
+
 class ff_neural_net:
-    def __init__(self,training_set,test_set,outputs,num_hidden_layers,num_hidden_nodes):
+    """
+        A class used to represent a Feed Forward Neural Network
+
+        Attributes
+        ----------
+        training_set: The training values
+        test_set: The values to test the model with
+        outputs: The number of outputs
+        num_hidden_layers: The number of hidden layers
+        num_hidden_nodes: The number of hidden nodes
+
+        Methods
+        -------
+        dot(x,w,b)
+            Returns the dot product between vector x and a weight vector w with a bias addition b.
+        sigmoid(self, x, isHyper=False)
+            Returns the vector x after being affected by a sigmoid function
+        train(training_rate, momentum)
+
+        test()
+
+        feed_forward(j)
+
+        back_prop(j, delta, training_rate, momentum)
+
+        activation(i, j)
+        """
+
+    def __init__(self, training_set, test_set, outputs, num_hidden_layers, num_hidden_nodes):
         self.training_set = training_set
         self.test_set = test_set
         self.outputs = outputs
         self.num_hidden_layers = num_hidden_layers
         self.num_hidden_nodes = num_hidden_nodes
-        #TODO
-        #create network
+        # TODO
+        # create network
 
-        #initialize weights to zero
+        # initialize weights to zero
 
-    def train(self,training_rate,momentum):
-        #TODO
+    def dot(self, x, w, b):
+        """Apply a dot product with a weight value and add a bias onto a vector x
+        :param x: The vector to have a weight applied to it. Must be the same size as w.
+        :param w: The weight vector to apply to an input vector. Must be the same size as x.
+        :param b: The bias value to be added to dot product
+        :return: weighted vector x
+        """
+        for i in range(len(x)):
+            x[i] = x[i] * w[i] + b
+        return x
+
+    def sigmoid(self, x, isHyper=False):
+        """Apply a sigmoid function onto a vector x
+        :param x: The input vector to have the sigmoid function applied to it.
+        :param isHyper: A binary value with default value false. If False, then use Logistic sigmoid function. If True, then use hyperbolic sigmoid function.
+        :return: Returns the vector after having the sigmoid function applied
+        """
+        # Logistic Sigmoid Function
+        if isHyper is False:
+            pass
+        # Hyperbolic Tangent Sigmoid Function
+        else:
+            pass
+
+    def train(self, training_rate, momentum):
+        pass
+        # TODO
 
     def test(self):
         result = None
-        #TODO
+        # TODO
         return result
 
     def feed_forward(self, j):
-        #TODO
+        pass
+        # TODO
 
-    def back_prop(self, j, delta, training_rate, momentum)
+    def back_prop(self, j, delta, training_rate, momentum):
         result = None
-        #TODO
+        # TODO
         return result
 
     def activation(self, i, j):
         result = None
-        #TODO
+        # TODO
         return result
+
--- RBF.py
+++ RBF.py
@@ -8,7 +8,27 @@ import KNN
 import FFN
 
 class rb_neural_net:
+    """
+        A class used to represent a Radial Basis Function Neural Network
+
+        Attributes
+        ----------
+        training_set: The set of values to train the model
+        test_set:
+        outputs:
+        gaussians:
+
+        Methods
+        -------
+
+    """
     def __init__(self,training_set,test_set,outputs,gaussians):
+        """
+        :param training_set:
+        :param test_set:
+        :param outputs:
+        :param gaussians:
+        """
         self.training_set = training_set
         self.test_set = test_set
         self.outputs = outputs
@@ -17,14 +37,29 @@ class rb_neural_net:
         #TODO
         
     def train(self, k, momentum):
+        """
+        :param k:
+        :param momentum:
+        :return:
+        """
         #TODO
+        pass
 
     def test(self, k):
+        """
+        :param k:
+        :return:
+        """
         result = None
         #TODO
         return result
 
     def activation(self, i, j):
+        """
+        :param i:
+        :param j:
+        :return:
+        """
         result = None
         #TODO
         return result
