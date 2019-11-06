e612a5a37fffe820e4a8c8243215b4cb8decaab1
Alex Alvarez <doubleabatteryalvarez@gmail.com>
2019-11-05 18:16:58 -0700

    rbf functionality
    a basic, unimplemented rbfn
    uses only one output

--- RBF.py
+++ RBF.py
@@ -6,6 +6,7 @@
 
 import KNN
 import FFN
+import random
 
 class rb_neural_net:
     """
@@ -24,49 +25,139 @@ class rb_neural_net:
     """
     def __init__(self,training_set,test_set,outputs,gaussians):
         """
-        :param training_set:
-        :param test_set:
-        :param outputs:
-        :param gaussians:
+        :training_set: The training set
+        :test_set: The test set
+        :outputs: The number of outputs
+        :gaussians: The number of gaussian functions
         """
         self.training_set = training_set
         self.test_set = test_set
         self.outputs = outputs
-        self.gaussians = gaussians
+        self.weights = [[]]
 
-        #TODO
+        for j in range(0, outputs):
+            weights.append(m)
+            for i in range(0,gaussians):
+                weights[j].append(random())
 
-    def gaussian_rbf(x,u,sigma):
-        rbf = (-((x-u)**2 )/(2* sigma**2))
-        return rbf
-    
-    def train(self, k, momentum):
-        """
-        :param k:
-        :param momentum:
-        :return:
-        """
-        #TODO
-        pass
 
-    def test(self, k):
-        """
-        :param k:
-        :return:
-        """
-        result = None
-        #TODO
-        return result
+        #find gaussians unsupervised
+        knn_instance = k_nearest_neighbor()
+        self.gaussians = knn_instance.kMeans(training_set, gaussians)
 
-    def activation(self, i, j):
+        return
+
+    def run_sample(sample):
+
+        kernal_values = []
+        target = sample[-1]
+        del sample[-1]
+
+        #calculate kernal values
+        for i in range(len(gaussians)):
+            kernal_values.append(gaussian(sample, i))
+
+        #calculate output values
+        output_values = []
+        for j in range(0,outputs):
+            output_values.append(0)
+            for i in range(len(kernal_values)):
+                output_values[j] += kernal_values[i] * weights[i][j]
+
+        return kernal_values, output_values
+
+    def train(self, learning_rate):
+        """
+        :learning_rate: The learning rate for training this net
+        """
+        #repeat until convergence
+        change = 1
+        while change > 0.01:
+
+            sample = training_set[random.randrange(len(training_set))] #choose sample at random
+            target = sample[-1]
+
+            kernal_values, output_values = run_sample(sample)
+
+            #apply gradient descent and track change in weights
+            ####I'm not super sure how he wants us to handle having multiple outputs for approximation so here I just use 1####
+            change = math.sqrt(vector_magnitude_squared(gradient_descent(kernel_values, output_values[0], target, learning_rate)))
+
+        return
+
+    def test(self):
+        """
+        :return: average mean squared error of test set
+        """
+        mean_sqr_err = 0
+
+        for x in range(len(test_set)):
+            k, o = run_sample(test_set[x])
+            err = test_set[x][-1] - o[0]
+            mean_sqr_err += err * err
+
+        mean_sqr_err /= len(test_set)
+
+        return mean_sqr_err
+
+
+    def gaussian(self, input, i, sigma):
         """
         :param i:
         :param j:
         :return:
         """
-        result = None
+        diffVect = vector_subtract(input, gaussians[i]);
+        diffMagSqr = vector_magnitude_squared(diffVect)
+        sigma = 1
+
+        result = math.exp(diffVect/(-2*sigma))
         #TODO
         return result
 
-        
-    
+    def gradient_descent(kernal_values, predicted_value, target_value, learning_rate):
+
+        error = -2*(target_value - predicted_value)
+        weight_change = []
+        for i in range(len(kernal_values)):
+            weight_change.append(error * kernal_values[i] * learning_rate)
+
+        for i in range:
+            weights[0] = vector_add(weights[0], weight_change)
+
+        return weight_change
+
+    def vector_add(x, y):
+        """
+        Vector x + Vector y
+        """
+        if (len[x] != len[y]):
+            return
+
+        z = []
+        for i in range(len(x)):
+            z.append(x[i]+y[i])
+
+        return z
+
+    def vector_subtract(x, y):
+        """
+        Vector x - Vector y
+        """
+        z = []
+        if len(x) != len(y):
+            return
+        for i in range(len(x)):
+            z.append(x[i]-y[i])
+
+        return z
+
+    def vector_magnitude_squared(x):
+        """
+        ||Vector x||^2
+        """
+        mag = 0
+        for i in range(len(x)):
+            mag += x[i] * x[i]
+
+        return mag
