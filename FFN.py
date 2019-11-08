85f65fbe511fd9f1aed1338dd5dc9ebc1e14e0c1
Greg Martin <theoggreg@gmail.com>
2019-11-07 18:22:40 -0700

    update

--- FFN.py
+++ FFN.py
@@ -30,19 +30,21 @@ class FeedForwardNeuralNetwork:
         return error*deriv
 
     def change_weights(self, layer, delta, weights, momentum):
-        return layer.delta * weights.T * momentum
+        return layer.delta * weights * momentum
 
     def backprop(self, data, correct_answer, momentum, output):
-        for i in reversed(range(len(self.total_layers)-1)):
+        for i in reversed(range(len(self.total_layers))):
+            print(i)
             layer = self.total_layers[i]
-            if(i==0):
+            if(i==len(self.total_layers)-1):
+                print(i)
                 layer.error = self.find_error(correct_answer,output)
                 layer.delta = layer.sigmoid_deriv(output)*layer.error    
             else:
                 next_layer = self.total_layers[i+1]
                 layer.error = np.dot(layer.weights.T, next_layer.delta)
                 layer.delta = layer.error*layer.already_activated
-            layer.weights += change_weights(layer, layer.delta, layer.weights, momentum)
+            layer.weights += self.change_weights(layer, layer.delta, layer.weights, momentum)
             print(layer.delta)
         #TODO 
     def train(self, net, actual, momentum, max_iterations):
@@ -78,7 +80,7 @@ class Layer:
 #TODO
 tData = pre_processing.pre_processing("data/car.data")
 trainData = dataset.dataset(tData.getData())
-x=np.array(trainData.getTrainingSet())
+x=np.array(trainData.getTrainingSet(0))
 
 #Simple test case. AND gate
 x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
