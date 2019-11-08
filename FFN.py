8b8fe2a5b850eeadb2e8ac29121f34b08e8fbcc0
Greg Martin <theoggreg@gmail.com>
2019-11-07 18:09:54 -0700

    Update FFN.py

--- FFN.py
+++ FFN.py
@@ -29,21 +29,31 @@ class FeedForwardNeuralNetwork:
     def calc_delta(self, error, deriv):
         return error*deriv
 
-    def change_weights(self, layer, delta, inp, momentum):
-        return layer.delta * inp.T * momentum
- 
+    def change_weights(self, layer, delta, weights, momentum):
+        return layer.delta * weights.T * momentum
+
     def backprop(self, data, correct_answer, momentum, output):
-        pass
+        for i in reversed(range(len(self.total_layers)-1)):
+            layer = self.total_layers[i]
+            if(i==0):
+                layer.error = self.find_error(correct_answer,output)
+                layer.delta = layer.sigmoid_deriv(output)*layer.error    
+            else:
+                next_layer = self.total_layers[i+1]
+                layer.error = np.dot(layer.weights.T, next_layer.delta)
+                layer.delta = layer.error*layer.already_activated
+            layer.weights += change_weights(layer, layer.delta, layer.weights, momentum)
+            print(layer.delta)
         #TODO 
     def train(self, net, actual, momentum, max_iterations):
         mse = 1
         count = 0
-        while(mse > 0.1):
+        #while(mse > 0.1):
+        for i in range(500):
             for j in range(len(net)):
                 output = self.feed_forward(net[j])
                 self.backprop(net[j], actual[j], momentum, output)
             mse = self.MSE(net, actual)
-
             count+=1
 
 class Layer:
