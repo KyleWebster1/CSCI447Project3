Added regression and classification. It works for any input layer, but does not perform well. Next step is to verify calculation correctness and fix issue where weights are pushed to extremely large numbers after 2nd iteration

FFN.py
@@ -27,7 +27,22 @@ class FeedForwardNeuralNetwork:
     def sigmoid_deriv(self, x):
         s = self.sigmoid(x)
         return s * (1 - s)
+    def regressionPred(self, x):
+        pred = []
+        #print(x)
+        for point in x:
+            p = self.sigmoid(self.feedForward(point))
+            pred.append(np.average(p))
+        #print(pred)
+        #print(self.weight)
+        return pred
 
+    def classificationPred(self, x):
+        pred = []
+        for point in x:
+            p = self.feedForward(point)
+            pred.append(np.argmax(p))
+        return pred
     def MSE(self, pred, actual):
         return np.mean(np.square(actual - pred))
 
@@ -44,12 +59,13 @@ class FeedForwardNeuralNetwork:
         self.H[0] = x
         for i in range(self.numberLayers):
             self.A[i + 1] = np.matmul(self.H[i], self.weight[i + 1]) + self.bias[i + 1]
-            self.H[i + 1] = self.sigmoid(self.A[i + 1])
+            self.H[i + 1] = self.sigmoid(np.dot(self.H[i], self.weight[i + 1]) + self.bias[i + 1])
         self.A[self.numberLayers + 1] = np.matmul(self.H[self.numberLayers], self.weight[self.numberLayers + 1]) + self.bias[self.numberLayers + 1]
         self.H[self.numberLayers + 1] = self.sigmoid(self.A[self.numberLayers + 1])
         return self.H[self.numberLayers + 1]
 
     def backprop(self, x, y):
+        self.feedForward(x)
         self.dW = {}
         self.dB = {}
         self.dH = {}
@@ -61,28 +77,23 @@ class FeedForwardNeuralNetwork:
             self.dB[k] = self.dA[k]
             self.dH[k - 1] = np.matmul(self.dA[k], self.weight[k].T)
             self.dA[k - 1] = np.multiply(self.dH[k - 1], self.sigmoid_deriv(self.H[k - 1]))
-# class Layer:
-#     def __init__(self, num_inputs, num_neurons):
-#         self.weights = np.random.rand(num_inputs, num_neurons)
-#         self.bias = np.random.rand(num_neurons)
-#         self.already_activated = None
-#         self.error = None
-#         self.delta = None
-#
-#     def activate_function(self, x):
-#         r = np.dot(x, self.weights) + self.bias
-#         self.already_activated = self.sigmoid(r)
-#         return self.already_activated
-#
-#     def sigmoid(self, r):
-#         return 1 / (1 + np.exp(-r))
-#
-#     def sigmoid_deriv(self, r):
-#         return r * (1 - r)
 
-tData = pre_processing.pre_processing("data/car.data")
+    def update(self, eda, x, y):
+        ffn.backprop(x, y)
+        m = x.shape[1]
+        for i in range(self.numberLayers + 1):
+            self.weight[i + 1] -= eda * (self.dW[i + 1] / m)
+            temp = eda * (self.dB[i + 1] / m)
+            self.bias[i + 1] -= temp[i]
+            # print(self.weight)
+            # print()
+            # print(self.bias)
+            # print()
+
+tData = pre_processing.pre_processing("data/machine.data")
 trainData = dataset.dataset(tData.getData())
 original=np.array(trainData.getTrainingSet(0))
+test = np.array(trainData.getTestSet(0))
 x = original[:,:-1]
 y = original[:,-1]
 xsize = x.shape #6 is input nodes Last column is correct_answer
@@ -92,10 +103,48 @@ ysize = np.unique(y).shape
 #y = np.array([[0], [1], [1], [1]])
 ffn = FeedForwardNeuralNetwork(xsize[1], ysize[0], [xsize[1]]*2)
 #print(ffn.feedForward(x))
-print(ffn.weight)
-ffn.feedForward(x)
-ffn.backprop(x,y)
-print(ffn.weight)
+#print(ffn.weight)
+
+for i in range (2000):
+    ffn.update(.1, x, y)
+#print(ffn.weight)
+
+
+#print(y)
+#clas = ffn.classificationPred(x)
+#print(clas)
+reg = ffn.regressionPred(x)
+print(ffn.MSE(y, reg))
+#print(ffn.accuracy(y, clas))
+
+tData = pre_processing.pre_processing("data/segmentation.data")
+trainData = dataset.dataset(tData.getData())
+original=np.array(trainData.getTrainingSet(0))
+test = np.array(trainData.getTestSet(0))
+x = original[:,:-1]
+y = original[:,-1]
+xsize = x.shape #6 is input nodes Last column is correct_answer
+ysize = np.unique(y).shape
+#Simple test case. AND gate
+#x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+#y = np.array([[0], [1], [1], [1]])
+ffn = FeedForwardNeuralNetwork(xsize[1], ysize[0], [xsize[1]]*2)
+#print(ffn.feedForward(x))
+#print(ffn.weight)
+
+for i in range (2000):
+    ffn.update(.1, x, y)
+#print(ffn.weight)
+
+
+#print(y)
+clas = ffn.classificationPred(x)
+#reg = ffn.regressionPred(x)
+#print(reg)
+#print(ffn.MSE(y, reg))
+print(ffn.accuracy(y, clas))
+
+
 """
 ffn.add(Layer(size[1], size[1]))
 ffn.add(Layer(size[1], size[1]))