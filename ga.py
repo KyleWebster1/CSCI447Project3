ef83155821c7f0df9c21a08947e4b1791b052236
m-wintersteen <muniwintersteen@gmail.com>
2019-12-09 09:37:56 -0700

    updating GA

--- ga.py
+++ ga.py
@@ -5,137 +5,158 @@ import pre_processing
 import dataset
 import numpy as np
 
-#Current Fitness Eval is just for testing. Will be updated to be
-#related to the error of the network
-def evalFit(chrm):
-    ideal = [4,4,4,4]
+class GA:
+    def __init__(self, dataset, isRegression):
 
-    meanOfMeans = 0
+        #store dataset
+        self.dataset = dataset
 
-    for i in range(len(chrm)):
-        meanOfMeans += evalSingleFit(chrm[i])
-    return meanOfMeans/(len(chrm))
+        #Create neural net to be trained
+        if (isRegression):
+            self.net = FFN.FeedForwardNeuralNetwork(len(dataset.training_set[0][0]) - 1, 2, dataset.getNumClasses())
+        else:
+            self.net = FFN.FeedForwardNeuralNetwork(len(dataset.training_set[0][0]) - 1, 2, 1)
 
-def evalSingleFit(chrm):
-    ideal = [4,4,4,4]
-    mean = 0
-    for i in range(len(chrm)):
-        mean += abs(chrm[i]-ideal[i])
-    return mean/len(chrm)
-
-def sort(chrm):
-    s = chrm
+        t = 0
     
-    for i in range(len(s)):
-        for j in range(len(s)-i):
-            if j+1 < len(s):
-                if evalSingleFit(s[j]) > evalSingleFit(s[j+1]):
-                    #swap
-                    temp = s[j]
-                    s[j] = s[j+1]
-                    s[j+1] = temp
-    return s
+        #init population
+        pop = []
+        data = []
 
-#select finds parents that will be used to make the next gen
-def select(chrm):
-    #sort by fitness
-    sortedChrm = sort(chrm)
-    #select the top half
-    selectNum = math.floor(len(chrm)/2)
-    select = []
-    for i in range(selectNum):
-        select.append(sortedChrm[i])
-                
-    return select
-
-def unique(p): 
-    unique_p = []
-    
-    for x in p: 
-        # check if exists in unique_p or not 
-        if x not in unique_p: 
-            unique_p.append(x)
-            
-    return unique_p
-
-def uniformCrossover(chrm):
-    newChrm = []
-    #crossover each bit randomly
-    for i in range(len(chrm)):
-        for k in range(len(chrm)):
-            p1 = chrm[i]
-            p2 = chrm[k]
-            for x in range(len(p1)):
-                if bool(random.getrandbits(1)) and p1[x] != p2[x]:
-                    temp = p1[x]
-                    p1[x] = p2[x]
-                    p2[x] = temp
-
-                    newChrm.append(p1)
-                    newChrm.append(p2)
-            
-
-            chrm[i] = p1
-            chrm[k] = p2
-    newChrm = unique(newChrm)
-
-    return newChrm
-
-def singleCrossover(chrm):
-    newChrm = []
-    #crossover each bit randomly
-    for i in range(len(chrm)):
-        for k in range(i+1,len(chrm)):
-            
-            pt = random.randint(1, len(chrm[i]))
-
-            newChrm.append(chrm[i][:pt]+chrm[k][pt:])
-    newChrm = unique(newChrm)
-    return newChrm
-
-#recombine uses the parents to make new children
-def recombine(chrm):
-    #newChrm = uniformCrossover(chrm)
-
-    newChrm = singleCrossover(chrm)
-    
-                
-    return newChrm
-
-def mutate(chrm):
-    newChrm = chrm
-
-    for i in range(len(newChrm)):
-        temp = newChrm[i]
-        for j in range(len(temp)):
-            creep = random.gauss(0,0.01)
-            temp[j] = round(temp[j] + creep,3)
+        for i in range(50):
+            data.append([random.randint(-3, 3),random.randint(-3, 3),random.randint(-3, 3),random.randint(-3, 3)])
         
-        newChrm[i] = temp
-    return newChrm
+        pop.append(data)
 
-def replace(children,parents):
-    newpop = parents
+        #init fitness
+        fit = []
+        fit.append(self.evalFit(pop[t]))
 
-    for c in children:
-        newpop.append(c)
+        while(self.evalFit(pop[t]) > .05):
+            t += 1
+            c = self.select(pop[t-1])
+            cp = self.recombine(c)
+            cpp = self.mutate(cp)
+            newpop = self.replace(cpp,pop[t-1])
+            pop.append(newpop)
+            fit.append(self.evalFit(pop[t]))
 
-    newpop = sort(newpop)
 
-    return newpop[:50]
+        print(pop[t])
+        print(fit[t])
+        print("# of iterations: ",t)
+        print("# of unique indiv from pop: ",len(self.unique(pop[t])))
+        pop[t] = self.sort(pop[t])
+        print(pop[t][:1])
+        print("//////////")        
+
+
+        
+    def evalFit(self,chrm):
+        ideal = [4,4,4,4]
+
+        meanOfMeans = 0
+
+        for i in range(len(chrm)):
+            meanOfMeans += self.evalSingleFit(chrm[i])
+        return meanOfMeans/(len(chrm))
+
+    def evalSingleFit(self,chrm):
+        ideal = [4,4,4,4]
+        mean = 0
+        for i in range(len(chrm)):
+            mean += abs(chrm[i]-ideal[i])
+        return mean/len(chrm)
+
+    def sort(self,chrm):
+        s = chrm
+        
+        for i in range(len(s)):
+            for j in range(len(s)-i):
+                if j+1 < len(s):
+                    if self.evalSingleFit(s[j]) > self.evalSingleFit(s[j+1]):
+                        #swap
+                        temp = s[j]
+                        s[j] = s[j+1]
+                        s[j+1] = temp
+        return s
+
+    #select finds parents that will be used to make the next gen
+    def select(self,chrm):
+        #sort by fitness
+        sortedChrm = self.sort(chrm)
+        #select the top half
+        selectNum = math.floor(len(chrm)/2)
+        select = []
+        for i in range(selectNum):
+            select.append(sortedChrm[i])
+                    
+        return select
+
+    def unique(self, p): 
+        unique_p = []
+        
+        for x in p: 
+            # check if exists in unique_p or not 
+            if x not in unique_p: 
+                unique_p.append(x)
+                
+        return unique_p
+
+    def uniformCrossover(self, chrm):
+        newChrm = []
+        #crossover each bit randomly
+        for i in range(len(chrm)):
+            for k in range(len(chrm)):
+                p1 = chrm[i]
+                p2 = chrm[k]
+                for x in range(len(p1)):
+                    if bool(random.getrandbits(1)) and p1[x] != p2[x]:
+                        temp = p1[x]
+                        p1[x] = p2[x]
+                        p2[x] = temp
+
+                        newChrm.append(p1)
+                        newChrm.append(p2)
+                
+
+                chrm[i] = p1
+                chrm[k] = p2
+        newChrm = unique(newChrm)
+
+        return newChrm
+
+    #recombine uses the parents to make new children
+    def recombine(self, chrm):
+        #newChrm = uniformCrossover(chrm)
+
+        newChrm = self.singleCrossover(chrm)
+                    
+        return newChrm
+
+    def mutate(self, chrm):
+        newChrm = chrm
+
+        for i in range(len(newChrm)):
+            temp = newChrm[i]
+            for j in range(len(temp)):
+                creep = random.gauss(0,0.01)
+                temp[j] = round(temp[j] + creep,3)
+            
+            newChrm[i] = temp
+        return newChrm
+
+    def replace(self,children,parents):
+        newpop = parents
+
+        for c in children:
+            newpop.append(c)
+
+        newpop = sort(newpop)
+
+        return newpop[:50]
 
 def main():
-    tData = pre_processing.pre_processing("data/machine.data")
-    trainData = dataset.dataset(tData.getData())
-    original=np.array(trainData.getTrainingSet(0))
-    test = np.array(trainData.getTestSet(0))
-    x = original[:,:-1]
-    y = original[:,-1]
-    xsize = x.shape #6 is input nodes Last column is correct_answer
-    ysize = np.unique(y).shape
-    
-    ffn = FFN.FeedForwardNeuralNetwork(xsize[1], ysize[0], [xsize[1]]*2)
-    
     layerData = []
     layerWeightData = []
     weightData = []
@@ -154,38 +175,20 @@ def main():
     print(reg)
     print(y)
     print(ffn.MSE(y, reg))
-    
-    t = 0
-    
-    #init population
-    pop = []
-    data = []
 
-    for i in range(50):
-        data.append([random.randint(-3, 3),random.randint(-3, 3),random.randint(-3, 3),random.randint(-3, 3)])
-    
-    pop.append(data)
+classification = ["data/car.data","data/segmentation.data","data/abalone.data"]
+regression = ["data/forestfires.csv","data/machine.data","data/winequality-red.csv","data/winequality-white.csv"]
 
-    #init fitness
-    fit = []
-    fit.append(evalFit(pop[t]))
+for file in classification:
+    tData = pre_processing.pre_processing(file)
+    trainData = dataset.dataset(tData.getData())
+    print (file)
+    print ("Num classes: " + str(trainData.getNumClasses()))
+    ga = GA(trainData, False)
+    ga.run()
 
-    while(evalFit(pop[t]) > .05):
-        t += 1
-        c = select(pop[t-1])
-        cp = recombine(c)
-        cpp = mutate(cp)
-        newpop = replace(cpp,pop[t-1])
-        pop.append(newpop)
-        fit.append(evalFit(pop[t]))
-
-
-    print(pop[t])
-    print(fit[t])
-    print("# of iterations: ",t)
-    print("# of unique indiv from pop: ",len(unique(pop[t])))
-    pop[t] = sort(pop[t])
-    print(pop[t][:1])
-    print("//////////")        
-
-main()
+for file in regression:
+    tData = pre_processing.pre_processing(file)
+    trainData = dataset.dataset(tData.getData())
+    ga = GA(trainData, True)
+    ga.run()
